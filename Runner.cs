716c68fda65b4cb06339d5d2cd6705b03660e138
Steve Desmond <steve@vtsv.ca>
2015-10-12 09:11:46 -0400

    add initial hammer

---
+++ Runner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace LoadTest
+{
+    public class Runner
+    {
+        private readonly Uri url;
+        private readonly int hammers;
+        private int done;
+        private double total;
+
+        public double Average => total / done;
+
+        public Runner(Uri url, int hammers)
+        {
+            this.url = url;
+            this.hammers = hammers;
+        }
+
+        public void Run()
+        {
+            for (var x = 0; x < hammers; x++)
+            {
+                var w = new Worker(url);
+                w.OnComplete += doMath;
+                var t = new Thread(w.Run);
+                t.Start();
+            }
+        }
+
+        private void doMath(object returned, EventArgs e)
+        {
+            var length = (double)returned;
+            total += length;
+            Interlocked.Increment(ref done);
+        }
+
+        public bool Complete()
+        {
+            return done == hammers;
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace LoadTest
+{
+
+    class Worker
+    {
+        private readonly Uri url;
+        public event EventHandler OnComplete;
+
+        public Worker(Uri url)
+        {
+            this.url = url;
+        }
+
+        public void Run()
+        {
+            var wc = new WebClient();
+            var start = DateTime.Now;
+            wc.DownloadString(url);
+            var end = DateTime.Now;
+
+            var length = (end - start);
+            OnComplete?.Invoke(length.TotalMilliseconds, null);
+        }
+    }
+}
