88b125de562015b2a8fbf37e9552cf46d055beff
Steve Desmond <steve@vtsv.ca>
2017-04-25 10:37:19 -0400

    add helper scripts

--- build.cmd
+++ build.cmd
@@ -1,5 +1,3 @@
-del /s /q bin
-dotnet restore common drill hammer
-dotnet build common -o bin -f net461 -c Release
-dotnet build drill  -o bin -f net461 -c Release
-dotnet build hammer -o bin -f net461 -c Release
+npm install
+dotnet restore
+dotnet build -c Release
---
+++ drill.cmd
@@ -0,0 +1,1 @@
+dotnet run --project src\LoadTestToolbox.csproj -c Release drill %*
