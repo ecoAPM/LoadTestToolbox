1708ff63a8bec71ae3bbfd3b35511e406ba5bcf4
Steve Desmond <steve@vtsv.ca>
2021-09-04 13:39:50 -0400

    Convert FormatTime nested ternaries to switch statement

--- src/Wielder.cs
+++ src/Wielder.cs
@@ -21,13 +21,15 @@ namespace LoadTestToolbox
 
 		public abstract Task<IDictionary<uint, double>> Run();
 
-		protected static string FormatTime(double ms)
-			=> ms < 1
-				? Math.Round(ms * 1000) + " μs"
-				: ms < 10
-					? Math.Round(ms, 2) + " ms"
-					: ms < 100
-						? Math.Round(ms, 1) + " ms"
-						: Math.Round(ms) + " ms";
+		public static string FormatTime(double ms)
+		{
+			return ms switch
+			{
+				< 1 => Math.Round(ms * 1000) + " μs",
+				< 10 => Math.Round(ms, 2) + " ms",
+				< 100 => Math.Round(ms, 1) + " ms",
+				_ => Math.Round(ms) + " ms"
+			};
+		}
 	}
 }
