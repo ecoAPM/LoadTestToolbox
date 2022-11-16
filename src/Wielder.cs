fa6b1c3922e59f2137c574681503f238a8afc693
Steve Desmond <Steve@ecoAPM.com>
2022-11-16 15:53:39 -0500

    Remove unused FormatTime method

--- src/Wielder.cs
+++ src/Wielder.cs
@@ -6,15 +6,4 @@ public abstract class Wielder<T, R> where T : Tool<R>
 
 	public IDictionary<uint, R> Run()
 		=> _tool.Run();
-
-	public static string FormatTime(double ms)
-	{
-		return ms switch
-		{
-			< 1 => Math.Round(ms * 1000) + " μs",
-			< 10 => Math.Round(ms, 2) + " ms",
-			< 100 => Math.Round(ms, 1) + " ms",
-			_ => Math.Round(ms) + " ms"
-		};
-	}
 }
